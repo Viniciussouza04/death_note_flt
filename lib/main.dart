f4417aa4dd6540eccbe44aad8462992b9774fd48
Vinicius Antonio de Souza <vs847540@gmail.com>
2023-03-02 00:07:40 -0300

    update
    penultimate step before finishing

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,15 @@ class meuApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(title: Text('World of Death Note')),
+            appBar: AppBar(title: Text('World of Death Note'), backgroundColor: Color.fromARGB(255, 3, 3, 3),),
             body: Column(
               children: [
                 Image.network('https://metagalaxia.com.br/wp-content/uploads/2020/04/er4khNt-1.jpg'),
+                // Info row
                 Container(
-                  padding: EdgeInsets.all(20),
+                  color: Color.fromARGB(255, 209, 209, 209),
+                  padding: EdgeInsets.all(18),
+                  // Left side
                   child: Row(
                     children:[
                       Expanded(
@@ -25,24 +28,61 @@ class meuApp extends StatelessWidget {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                            'Death note',
-                            style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold) 
+                              'Death Note',
+                              style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold) 
                             ),
+                            Padding(padding: EdgeInsets.all(1.5)),
                             Text('Tóqui, Japão',
-                            style: TextStyle(                              
+                              style: TextStyle(                              
                               fontSize: 16,
-                              color: Colors.grey)
+                              color: Color.fromARGB(255, 214, 13, 13))
                             )
                           ],
                         ),
                       ),
+                      // Right side
                       Row(
                         children: [
-                          Icon(Icons.star, color: Colors.blue,),
-                          Text('9.984')
+                          Icon(Icons.star, color: Color.fromARGB(255, 214, 13, 13),),
+                          Text('9.84')
                         ],
+                      )
+                    ],
+                  ),
+                ),
+                // Info pages
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextButton(onPressed:(){},
+                        child: Column(
+                          children: [
+                            Icon(Icons.web, color: Color.fromARGB(255, 214, 13, 13),),
+                            Text("Site", style: TextStyle(color: Colors.black))
+                          ],
+                        ))
+                      ),
+                      Expanded(
+                        child: TextButton(onPressed:(){},
+                        child: Column(
+                          children: [
+                            Icon(Icons.gamepad, color: Color.fromARGB(255, 214, 13, 13),),
+                            Text("Jogar", style: TextStyle(color: Colors.black))
+                          ],
+                        ))
+                      ),
+                      Expanded(
+                        child: TextButton(onPressed:(){},
+                        child: Column(
+                          children: [
+                            Icon(Icons.download, color: Color.fromARGB(255, 214, 13, 13),),
+                            Text("Baixar", style: TextStyle(color: Colors.black))
+                          ],
+                        ))
                       )
                     ],
                   ),
